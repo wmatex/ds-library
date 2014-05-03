5530979ae8c7f1610f1ca4de54fecfdad85480be
Matej Vavrinec <wmatex@gmail.com>
2014-05-03 21:48:44 +0200

    + view for all titules and free prints

--- triggers.sql
+++ triggers.sql
@@ -8,17 +8,36 @@ $_$ LANGUAGE plpgsql;
 CREATE TRIGGER gen_bar_code BEFORE INSERT ON vytisk
 FOR EACH ROW EXECUTE PROCEDURE generate_bar_code();
 
+-- Vsechny tituly s poctem volnych vytisku
+CREATE OR REPLACE VIEW vw_titul AS
+SELECT t.id_titul, t.nazev, t.rok_vydani, t.vypujcni_doba_dny, t.cena, 
+z.nazev as zanr, v2.cnt - COALESCE(v.cnt, 0) as volne_vytisky
+FROM titul t
+INNER JOIN zanr z ON z.id_zanr = t.id_zanr
+LEFT OUTER JOIN (
+  SELECT vk.id_titul, COUNT(vp.*) as cnt
+  FROM vytisk vk
+  INNER JOIN vypujcka vp ON vk.id_vytisk = vp.id_vytisk
+  WHERE vp.je_vraceno = FALSE
+  GROUP BY vk.id_titul
+) v ON v.id_titul = t.id_titul
+INNER JOIN (
+  SELECT id_titul, COUNT(*) as cnt
+  FROM vytisk
+  GROUP BY id_titul
+) v2 ON v2.id_titul = t.id_titul;
+
 CREATE OR REPLACE FUNCTION vytvor_rezervaci(
   _id_uzivatel int, _id_titul int, _interval interval) RETURNS VOID AS $_$
 DECLARE
   _int interval;
   count int;
 BEGIN
-  SELECT COUNT(*) INTO count FROM vytisk
+  SELECT volne_vytisky INTO count FROM vw_titul
   WHERE id_titul = _id_titul;
   if count > 0 then
-    RAISE EXCEPTION 'Existuje volny vytisk'
-  end if
+    RAISE EXCEPTION 'Existuje volny vytisk';
+  end if;
   if _interval is null then
     _int := '1 year';
   else
